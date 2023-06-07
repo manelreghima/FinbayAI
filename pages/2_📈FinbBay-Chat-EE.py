3b630b38031dda0f1ba1e5f52a7b239365044fa9
manelreghima <manelreghima@gmail.com>
2023-06-07 02:04:31 +0100

    update EE chat

--- pages/2_📈FinbBay-Chat-EE.py
+++ pages/2_📈FinbBay-Chat-EE.py
@@ -12,13 +12,16 @@ from langchain.llms import OpenAI
 import numpy as np
 import pandas as pd
 
+st.set_page_config(
+    page_title="Finbay AI",
+    page_icon="data/finbay-logo.jpg",
+)
 # Load environment variables from .env file
 load_dotenv()
 # Access the API key
 api_key = os.environ["OPENAI_API_KEY"]
 openai.api_key = os.environ["OPENAI_API_KEY"]
 data = pd.read_csv('data/company_ticker.csv')
-
 llm = OpenAI(temperature=0)
 
 def extract_symbol(input):
@@ -33,10 +36,10 @@ def chat_query(prompt_prefix, text):
 
 def extract_company_name(input):
         # Generate a response
-    prompt =  'Väljavõte ettevõtte nimi sellest tekstist:' + input
+    prompt =  'Väljavõte ettevõtte nimest sellest tekstist:' + input
     return llm(prompt) 
 
-st.title("Tere tulemast FinbayAI")
+st.title("Tere tulemast FinbayAIsse")
 
 # Storing the chat
 if 'generated' not in st.session_state:
@@ -46,12 +49,10 @@ if 'past' not in st.session_state:
     st.session_state['past'] = []
 
 
-
 # container for text box
 container = st.container()
-questions=["Kui suur on ettevõtte DGR1R.RG turukapitalisatsioon?", 
-           "Kui suur on ettevõtte AUG1LS.VS tulevane keskmine kasumimarginaal?",
-           "Kes on ettevõtte LHV1T.TL tegevjuht?", "Kui suur on ettevõtte TSLA kasumimarginaal?"]
+questions=["Milline on DGR1R.RG turukapitalisatsioon?", "Milline on AUG1LS.VS tulevane keskmine rahaproduktiväärtus?",
+           "Kes on ettevõtte LHV1T.TL tegevjuht?", "Milline on ettevõtte TSLA kasumimarginaal?", "Milline on ettevõtte TSLA kasumimarginaal? "]
 
 def process_question(question):
     user_input = question
@@ -64,7 +65,7 @@ def process_question(question):
     
     #if symbol is not None:
     #symbol = symbol.strip()
-    #ticker_symbol = symbol 
+    
     ticker = yf.Ticker(symbol)    
     text = str(ticker.info)
     output = chat_query(user_input, text)
@@ -88,18 +89,19 @@ def get_graph(symbol):
     today = datetime.now()
     formatted_today = today.strftime('%Y-%m-%d')
     data = yf.download(symbol, interval = '1mo', start="2023-01-01", end=formatted_today)
-    fig1 = px.line(data_frame=data, x=data.index, y='Adj Close', title=str(symbol)+' Ajaloolised sulgemishinnad')
-    fig1.update_xaxes(title='Kuupäev')
-    fig1.update_yaxes(title='Hind')
+    fig1 = go.Figure()
+    fig1.add_trace(go.Scatter(x=data.index, y=data['Adj Close'], fill='tozeroy', name='Adj Close'))
+    fig1.update_layout(title=str(symbol)+' Ajaloolised sulgemishinnad', xaxis_title='Kuupäev', yaxis_title='Hind')
 
-    # Create figure for "Volume"
-    fig2 = px.line(data_frame=data, x=data.index, y='Volume', title=str(symbol)+' Ajalooline maht')
-    fig2.update_xaxes(title='Kuupäev')
-    fig2.update_yaxes(title='Köide')
+    fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(x=data.index, y=data['Volume'], fill='tozeroy', name='Volume'))
+    fig2.update_layout(title=str(symbol)+' Ajalooline maht', xaxis_title='Kuupäev', yaxis_title='Köide')
+    
 
     # Display charts using Streamlit
     st.plotly_chart(fig1)
     st.plotly_chart(fig2)
+
 
 def get_market_data():
     
@@ -130,20 +132,22 @@ def get_market_data():
     })
     
     return market_data
+
+
 market_data=get_market_data()
 
 with container:
     for question in questions:
         if st.button(question):
             process_question(question)
-            
-    if st.button("Alusta uut vestlust"):
+    
+    if st.button("Alusta uut vest"):
         st.session_state.past.clear()
         st.session_state.generated.clear()
 
     with st.form(key='my_form', clear_on_submit=True):
-        user_input = st.text_input("Esitage küsimus:", key='input')
-        submit_button = st.form_submit_button(label='Saada')
+        user_input = st.text_input("Esita küsimus:", key='input')
+        submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
         process_question(user_input)
