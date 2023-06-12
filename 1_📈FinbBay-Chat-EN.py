c738fd3f51dde7f4403aec5e74b9f4dee90853d4
manelreghima <manelreghima@gmail.com>
2023-06-13 00:17:44 +0100

    add sidebar list

--- 1_📈FinbBay-Chat-EN.py
+++ 1_📈FinbBay-Chat-EN.py
@@ -203,3 +203,17 @@ fig = px.treemap(market_data, path=['sector', 'symbol'], values='market_cap',
 
             # Display the figure in Streamlit
 st.plotly_chart(fig)
+
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         icons=['house', 'camera fill', 'kanban', 'book','person lines fill'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
