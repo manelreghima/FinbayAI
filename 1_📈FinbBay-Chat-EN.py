aeb58489adb7022d268e165c6a9650c59168962b
manelreghima <manelreghima@gmail.com>
2023-06-13 00:26:21 +0100

    change EN chat

--- 1_📈FinbBay-Chat-EN.py
+++ 1_📈FinbBay-Chat-EN.py
@@ -207,13 +207,13 @@ st.plotly_chart(fig)
 from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
-                         icons=['Tesla', 'camera fill', 'kanban', 'book','person lines fill'],
+    choose = option_menu("800+ companies you can currently ask Finbay AI about.", ["About", "Photo Editing", "Project Planning", "Python e-Course", "Contact"],
+                         #icons=['Tesla', 'camera fill', 'kanban', 'book','person lines fill'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        #"icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "10px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {"background-color": "#02ab21"},
     }
     )
