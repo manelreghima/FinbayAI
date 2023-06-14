fcdfffc0b46e0eece1d345c8f035ccd93cb04e8c
manelreghima <manelreghima@gmail.com>
2023-06-14 11:54:08 +0100

    change Baltic Companies.py

--- pages/3_📊Baltic Companies.py
+++ pages/3_📊Baltic Companies.py
@@ -5,6 +5,8 @@ from langchain.llms import OpenAI
 from datetime import datetime
 import yfinance as yf
 import plotly.graph_objects as go
+from requests.exceptions import HTTPError
+
 
 def read_data():
     data = pd.read_csv('data/company_ticker.csv')
@@ -76,3 +78,75 @@ df_company = data[data['company']==choose]
 symbol = str(df_company['symbol2'].iloc[0])
 # Move get_graph(symbol) outside the sidebar
 get_graph(symbol)
+
+
+llm = OpenAI(temperature=0)
+
+def chat_query(prompt_prefix, text):
+        # Generate a response
+    prompt =  prompt_prefix + ': ' + text
+    return llm(prompt) 
+
+def extract_company_name(input):
+        # Generate a response
+    prompt =  'Extract company name from this text:' + input
+    return llm(prompt) 
+
+# container for text box
+container = st.container()
+questions=["What is the market cap of "+str(choose)+"?","What is the forward PE of "+str(choose)+"?",
+           "Who is the CEO of "+str(choose)+"?","What is the profit margin of "+str(choose)+"?"]
+
+
+def process_question(question):
+    data=read_data()
+    user_input = question
+    company_name = extract_company_name(user_input)
+    company_name = str(company_name).strip()
+    company_list = data['company'].values.tolist()
+   
+    if company_name in data['symbol1'].values:
+      df_company = data[data['symbol1']==company_name]
+      symbol = str(df_company['symbol2'].iloc[0])  
+    elif company_name in company_list:
+      df_company = data[data['company']==company_name]
+      symbol = str(df_company['symbol2'].iloc[0])     
+    else:
+        symbol=company_name
+
+    ticker = yf.Ticker(symbol)
+    try:
+        ticker_info = ticker.info
+        
+        if 'error' in ticker_info:
+            print(f"An error occurred for ticker symbol '{symbol}': {ticker_info['error']}")
+        else:
+            print(f"Ticker symbol '{symbol}' does not have an error in the info.")
+    except HTTPError as e:
+        print(f"Sorry, there is currently no data available for the company requested")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    text = str(ticker.info)
+    output = chat_query(user_input, text)
+
+    # Store the output
+    st.session_state.past.append(user_input)
+    #st.session_state.generated.append(question)  # Append the question
+    st.session_state.generated.append(output)
+
+with container:
+    for question in questions:
+        if st.button(question):
+            process_question(question)
+    
+    if st.sidebar.button("Start a New Chat"):
+        st.session_state.past.clear()
+        st.session_state.generated.clear()
+
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_input("Ask a question:", key='input')
+        submit_button = st.form_submit_button(label='Send')
+
+    if submit_button and user_input:
+        process_question(user_input)
