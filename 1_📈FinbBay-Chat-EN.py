dbdb287396f8279f32b3c730e5bfb60e50e9b593
manelreghima <manelreghima@gmail.com>
2023-06-17 16:22:49 +0100

    resize logos

--- 1_📈FinbBay-Chat-EN.py
+++ 1_📈FinbBay-Chat-EN.py
@@ -189,9 +189,9 @@ for image_path in logo_images:
     small_logo_images.append(resized_image)
 
 # Add small_logo_images to the table
-table[0].append(st.image(small_logo_images[0], use_column_width=True))
-table[0].append(st.image(small_logo_images[1], use_column_width=True))
-table[0].append(st.image(small_logo_images[2], use_column_width=True))
+table[0].append(st.image(small_logo_images[0]))
+table[0].append(st.image(small_logo_images[1]))
+table[0].append(st.image(small_logo_images[2]))
 
 table[1].append(st.button(questions[0]))
 table[1].append(st.button(questions[1]))
