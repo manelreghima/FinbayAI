0c492ed2cabc5bb89dcaad7b499b594d806a60fe
manelreghima <manelreghima@gmail.com>
2023-06-19 15:01:29 +0100

    update page3

--- pages/3_📊Baltic Companies.py
+++ pages/3_📊Baltic Companies.py
@@ -13,112 +13,151 @@ def read_data():
     data = pd.read_csv('data/company_ticker.csv')
     return data
 
-def chat_query(prompt_prefix, text):
-        # Generate a response
-    prompt =  prompt_prefix + ': ' + text
-    return llm(prompt) 
-
 def get_graph(symbol):
     today = datetime.now()
     formatted_today = today.strftime('%Y-%m-%d')
-    data = yf.download(symbol, interval='1mo', start="2023-01-01", end=formatted_today)
+    data = yf.download(symbol, interval = '1mo', start="2023-01-01", end=formatted_today)
     fig1 = go.Figure()
     fig1.add_trace(go.Scatter(x=data.index, y=data['Adj Close'], fill='tozeroy', name='Adj Close'))
-    fig1.update_layout(title=f'{symbol} Historical Close Prices', xaxis_title='Date', yaxis_title='Price')
+    fig1.update_layout(title=str(symbol)+' Historical Close Prices', xaxis_title='Date', yaxis_title='Price')
 
     fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=data.index, y=data['Volume'], fill='tozeroy', name='Volume'))
-    fig2.update_layout(title=f'{symbol} Historical Volume', xaxis_title='Date', yaxis_title='Volume')
+    fig2.update_layout(title=str(symbol)+' Historical Volume', xaxis_title='Date', yaxis_title='Volume')
+    
 
     # Display charts using Streamlit
     st.plotly_chart(fig1)
     st.plotly_chart(fig2)
 
+data=read_data()
+with st.sidebar:
+    choose = option_menu("Companies you can currently ask Finbay AI about.",
+                         ['APB Apranga',
+                        'Arco Vara AS',
+                        'Auga Group AB',
+                        'AS Baltika',
+                        "Coop Pank AS",
+                        'DelfinGroup AS',
+                        'AS Ekspress Grupp',
+                        'EfTEN Real Estate Fund III AS',
+                        'Enefit Green AS',
+                        'Grigeo AB',
+                        'AS Harju Elekter',
+                        'AS HansaMatrix',
+                        'Hepsor AS',
+                        'IPAS Indexo AS',
+                        'AB Ignitis grupe',
+                        'AB Klaipedos nafta',
+                        'AS LHV Group',
+                        'AB Linas Agro Group',
+                        'AS Merko Ehitus',
+                        'Nordecon AS',
+                        'Novaturas AB',
+                        'AS Pro Kapital Grupp',
+                        'AS PRFoods',
+                        'AB Panevezio Statybos Trestas',
+                        'AB Pieno Zvaigzdes',
+                        'Rokiskio Suris AB',
+                        'AB Siauliu Bankas',
+                        'SAF Tehnika A/S',
+                        'AS Silvano Fashion Group',
+                        'AS Tallink Grupp',
+                        'Telia Lietuva, AB',
+                        'Tallinna Kaubamaja Grupp AS',
+                        'AS Tallinna Sadam',
+                        'AS Tallinna Vesi',
+                        'AB Vilkyskiu pienine'],
+                         
+                         default_index=0,
+                         styles={
+                             "container": {"padding": "5!important", "background-color": "#1D1D1D"},
+                             "nav-link": {"text-align": "left", "margin": "0px", "--hover-color": "#262626"},
+                             "nav-link-selected": {"background-color": "#00A767"},
+                         })
+df_company = data[data['company']==choose]
+symbol = str(df_company['symbol2'].iloc[0])
+# Move get_graph(symbol) outside the sidebar
+#get_graph(symbol)
 
-def extract_company_name(input_text):
-    prompt = 'Extract company name from this text: ' + input_text
-    return llm(prompt)
 
+llm = OpenAI(temperature=0)
+
+def chat_query(prompt_prefix, text):
+        # Generate a response
+    prompt =  prompt_prefix + ': ' + text
+    return llm(prompt) 
+
+def extract_company_name(input):
+        # Generate a response
+    prompt =  'Extract company name from this text:' + input
+    return llm(prompt) 
+
+# container for text box
+container = st.container()
 
 def process_question(question):
+    data=read_data()
     user_input = question
-    company_name = extract_company_name(user_input).strip()
+    company_name = extract_company_name(user_input)
+    company_name = str(company_name).strip()
     company_list = data['company'].values.tolist()
-
+   
     if company_name in data['symbol1'].values:
-        df_company = data[data['symbol1'] == company_name]
-        symbol = str(df_company['symbol2'].iloc[0])
+      df_company = data[data['symbol1']==company_name]
+      symbol = str(df_company['symbol2'].iloc[0])  
     elif company_name in company_list:
-        df_company = data[data['company'] == company_name]
-        symbol = str(df_company['symbol2'].iloc[0])
+      df_company = data[data['company']==company_name]
+      symbol = str(df_company['symbol2'].iloc[0])     
     else:
-        symbol = company_name
+        symbol=company_name
 
     ticker = yf.Ticker(symbol)
     try:
         ticker_info = ticker.info
-
+        
         if 'error' in ticker_info:
-            st.error(f"An error occurred for ticker symbol '{symbol}': {ticker_info['error']}")
+            print(f"An error occurred for ticker symbol '{symbol}': {ticker_info['error']}")
         else:
-            st.info(f"Ticker symbol '{symbol}' does not have an error in the info.")
+            print(f"Ticker symbol '{symbol}' does not have an error in the info.")
     except HTTPError as e:
-        st.error("Sorry, there is currently no data available for the requested company.")
+        print(f"Sorry, there is currently no data available for the company requested")
     except Exception as e:
-        st.error(f"An error occurred: {e}")
-
+        print(f"An error occurred: {e}")
+    
     text = str(ticker.info)
     output = chat_query(user_input, text)
 
     # Store the output
     st.session_state.past.append(user_input)
+    #st.session_state.generated.append(question)  # Append the question
     st.session_state.generated.append(output)
 
-
-# Initialize Streamlit
-st.sidebar.title("Companies you can currently ask Finbay AI about.")
-data = read_data()
-choose = option_menu("Choose a company", data['company'].values.tolist())
-
-df_company = data[data['company'] == choose]
-symbol = str(df_company['symbol2'].iloc[0])
-
-# Move get_graph(symbol) outside the sidebar
-get_graph(symbol)
-
-# Initialize the OpenAI language model
-llm = OpenAI(temperature=0)
-
-# Initialize session state variables
-if 'past' not in st.session_state:
-    st.session_state.past = []
-if 'generated' not in st.session_state:
-    st.session_state.generated = []
-
-# Process predefined questions
 questions = [
-    "What is the market cap of " + choose + "?",
-    "What is the forward PE of " + choose + "?",
-    "Who is the CEO of " + choose + "?",
-    "What is the profit margin of " + choose + "?",
-    "What is the dividend rate and yield for " + choose + "?",
-    "How has the stock price of " + choose + " performed over the past year?"
+    "What is the market cap of "+str(choose)+"?",
+    "What is the forward PE of "+str(choose)+"?",
+    "Who is the CEO of "+str(choose)+"?",
+    "What is the profit margin of "+str(choose)+"?",
+    "What is the dividend rate and yield for "+str(choose)+"?",
+    "How has the stock price of "+str(choose)+" performed over the past year?"
 ]
 
+# Create columns dynamically
 num_columns = 3
 num_questions = len(questions)
 num_rows = (num_questions + num_columns - 1) // num_columns
 columns = st.columns(num_columns)
 
+# Add small_logo_images and buttons to the columns
 for i in range(num_questions):
     col_index = i % num_columns
     row_index = i // num_columns
 
     with columns[col_index]:
-        if st.button(questions[i]):
-            process_question(questions[i])
-
-# Process user input
+        
+        if columns[col_index].button(questions[i]):
+                process_question(questions[i])
+        
 container = st.container()
 with container:
     if st.sidebar.button("Start a New Chat"):
@@ -130,23 +169,22 @@ with container:
         submit_button = st.form_submit_button(label='Send')
 
     if submit_button and user_input:
-        process_question(user_input)
+        process_question(user_input)    
 
-# Display generated responses and corresponding questions
-if st.session_state.generated:
-    num_responses = len(st.session_state.generated)
-
+if st.session_state['generated']:
+    num_responses = len(st.session_state['generated'])
+    
     for i in reversed(range(num_responses)):
-        if i < len(st.session_state.generated):
-            symbol = extract_company_name(st.session_state.past[i])
-            message(st.session_state.generated[i].strip(), key=str(i))  # Display the answer without leading/trailing whitespace
-
-            if i < len(st.session_state.past):
+        if i < len(st.session_state['generated']):
+            symbol = extract_company_name(st.session_state['past'][i])
+            message(st.session_state['generated'][i].strip(), key=str(i))  # Display the answer without leading/trailing whitespace
+            
+            if i < len(st.session_state['past']):
                 ticker = yf.Ticker(symbol)
                 try:
                     ticker_info = ticker.info
                     get_graph(symbol)
                 except HTTPError as e:
-                    st.error("An HTTPError occurred:", e)
-
-                message(st.session_state.past[i], is_user=True, key=str(i) + '_user')  # Display the question
+                    print("An HTTPError occurred:", e)
+                
+                message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')  # Display the question
