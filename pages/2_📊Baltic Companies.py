d20f21a79e4091a16422e861db3d15444012cb9f
manelreghima <manelreghima@gmail.com>
2023-06-28 01:00:54 +0100

    adjust logo size

--- pages/2_📊Baltic Companies.py
+++ pages/2_📊Baltic Companies.py
@@ -24,7 +24,7 @@ def add_logo():
                 background-image: url(data:image/png;base64,{encoded_image});
                 background-repeat: no-repeat;
                 padding-top: 120px;
-                background-position: 60px 20px;
+                background-position: 70px 20px;
                 background-size: 180px;
             }}
             [data-testid="stSidebarNav"]::before {{
