f6dd1af1a0d2715ce3a38a9a71a164e241ba8892
manelreghima <manelreghima@gmail.com>
2023-06-30 23:14:43 +0100

    change logos

--- pages/2_📊Baltic Companies.py
+++ pages/2_📊Baltic Companies.py
@@ -73,7 +73,30 @@ def add_company_logo(company):
     with open('data/companies_logos/'+company+'.png', 'rb') as f:
         image_data = f.read()
 
-    st.markdown('<div class="center-image"></div>', unsafe_allow_html=True)
+    encoded_image = base64.b64encode(image_data).decode()
+    
+    # Create the CSS style with the encoded image as the background
+    css_style = f"""
+        <style>
+            [data-testid="stSidebarNav"] {{
+                background-image: url(data:image/png;base64,{encoded_image});
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 70px 20px;
+                background-size: 180px;
+            }}
+            [data-testid="stSidebarNav"]::before {{
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }}
+        </style>
+    """
+
+    # Apply the CSS style
+    st.markdown(css_style, unsafe_allow_html=True)
 
 
 
