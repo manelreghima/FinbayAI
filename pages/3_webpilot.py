0b49b2583f6a527d4f1e77ea1667ce97804811a7
manelreghima <manelreghima@gmail.com>
2023-07-19 11:55:40 +0100

    change page 3

--- pages/3_webpilot.py
+++ pages/3_webpilot.py
@@ -5,12 +5,14 @@ import os
 import streamlit as st
 
 from langchain.llms import OpenAI
+
 llm = OpenAI(temperature=0)
 
 def webpilot(input):
-            # Generate a response
-        prompt =  'Using WebPilot, give me the historical revenue from this page:' + input
-        return llm(prompt)
-response_json=webpilot('https://finance.yahoo.com/quote/LHV1T.TL/financials?p=LHV1T.TL')
-df = pd.read_json(response_json)
+    # Generate a response
+    prompt = 'Using WebPilot, give me the historical revenue from this page:' + input
+    return llm(prompt)
+
+response_json = webpilot('https://finance.yahoo.com/quote/LHV1T.TL/financials?p=LHV1T.TL')
+df = pd.DataFrame(response_json)
 st.write(df)
