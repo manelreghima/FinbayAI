d51b4eb974eef17563847cc60a048551cd450cd2
manelreghima <manelreghima@gmail.com>
2023-07-21 22:25:09 +0100

    change graph

--- pages/3_📊webpilot.py
+++ pages/3_📊webpilot.py
@@ -15,7 +15,7 @@ def webpilot(input):
 
 def graph(input):
     # Generate a response
-    prompt = 'based on this df plot a bar plot using daigr.am' + input
+    prompt = 'based on this dataframe plot a bar plot using daigr.am' + input
     return llm(prompt)
 # Page title
 st.title("WebPilot Revenue Viewer")
@@ -30,4 +30,4 @@ df = pd.read_json(response_json)
 # Display DataFrame
 st.write("Historical Revenue Data:")
 st.dataframe(df)
-graph(response_json)
+graph(webpilot(input_url))
