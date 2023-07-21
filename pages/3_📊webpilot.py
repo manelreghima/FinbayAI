3529e1ac5285056de6fe47f57e6fc8a245c3d539
manelreghima <manelreghima@gmail.com>
2023-07-21 22:29:51 +0100

    change code

--- pages/3_📊webpilot.py
+++ pages/3_📊webpilot.py
@@ -14,11 +14,7 @@ def webpilot(input):
 def graph(input):
     # Generate a response
     prompt = 'based on this dataframe plot a bar plot using daigr.am' + input
-    response = llm(prompt)  # Get the response from OpenAI
-    # Assuming the response is a JSON representation of a DataFrame, load it into a DataFrame
-    df = pd.read_json(response)
-    # Create and return the bar plot using pandas plotting function
-    return df.plot(x='x_column_name', y='y_column_name', kind='bar')
+    return llm(prompt)
 
 # Page title
 st.title("WebPilot Revenue Viewer")
@@ -28,12 +24,16 @@ input_url = 'https://finance.yahoo.com/quote/LHV1T.TL/financials?p=LHV1T.TL'
 
 # Retrieve historical revenue
 response_json = webpilot(input_url)
-df = pd.read_json(response_json)
+df = pd.read_json(response_json)  # Convert the response data to DataFrame
 
-# Display DataFrame
-st.write("Historical Revenue Data:")
-st.dataframe(df)
+# Check if DataFrame is valid and contains data
+if df.empty:
+    st.write("Error: Unable to fetch historical revenue data.")
+else:
+    # Display DataFrame
+    st.write("Historical Revenue Data:")
+    st.dataframe(df)
 
-# Display the bar plot
-st.write("Bar Plot:")
-st.pyplot(graph(input_url))
+    # Display the bar plot
+    st.write("Bar Plot:")
+    st.pyplot(graph(response_json))
