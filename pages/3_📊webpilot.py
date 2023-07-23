8b34977e4a04b57585c49562311eb08e6e8b4ef3
manelreghima <manelreghima@gmail.com>
2023-07-23 20:43:02 +0100

    change graphs

--- pages/3_📊webpilot.py
+++ pages/3_📊webpilot.py
@@ -10,13 +10,13 @@ def webpilot(input_url):
     # Generate a response
     prompt = 'Using WebPilot, give me the historical revenue in euro from this page as a json: ' + input_url
     response = llm.generate([prompt])
-    return response[0]  # Assuming the generate function returns a list
+    return response.get_text()  # hypothetical method to extract text from response
 
 def graph(df):
     # Generate a response
     prompt = 'Based on this dataframe, plot a bar plot using daigr.am: ' + df.to_json()
     figure = llm.generate([prompt])
-    return figure[0]  # Assuming the generate function returns a list
+    return figure.get_text()  # hypothetical method to extract text from response
 
 # Page title
 st.title("WebPilot Revenue Viewer")
