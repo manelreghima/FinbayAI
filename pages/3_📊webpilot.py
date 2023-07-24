72e5983dc39b5da90718ac2b7bef7e86062e6f1d
manelreghima <manelreghima@gmail.com>
2023-07-24 11:54:46 +0100

    change graph

--- pages/3_📊webpilot.py
+++ pages/3_📊webpilot.py
@@ -1,5 +1,4 @@
 from langchain.llms import OpenAI
-import os
 import streamlit as st
 import pandas as pd
 
@@ -8,14 +7,13 @@ llm = OpenAI(temperature=0)
 @st.cache
 def webpilot(input):
     # Generate a response
-    prompt = 'Using WebPilot, give me the historical revenue in euro from this page as a json ' + input
+    prompt = 'Using WebPilot, give me the historical revenue in euro from this page as a json' + input
     return llm(prompt)
 
 def graph(input):
     # Generate a response
-    prompt = 'based on this df plot a bar plot using daigr.am ' + input
+    prompt = 'based on this df plot a bar plot using daigr.am' + input
     return llm(prompt)
-
 # Page title
 st.title("WebPilot Revenue Viewer")
 
@@ -30,5 +28,9 @@ df = pd.read_json(response_json)
 st.write("Historical Revenue Data:")
 st.dataframe(df)
 
-# Plot the graph
-graph(response_json)
+import plotly.express as px
+
+fig = px.bar(df, x='Year', y='Revenue', title='Historical Revenue of AS LHV Group')
+
+# Display the plot in Streamlit
+st.plotly_chart(fig)
