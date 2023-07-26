0904aef5b03961e02be9b84d764742627e78213a
manelreghima <manelreghima@gmail.com>
2023-07-26 10:50:41 +0100

    change code

--- pages/3_📊webpilot.py
+++ pages/3_📊webpilot.py
@@ -16,10 +16,25 @@ def graph(input):
     prompt = 'based on this df plot a bar plot using daigr.am' + input
     return llm(prompt)
 
-def Cash_flow_statement(input):
+
+
+def Cash_flow_statement(input_url):
     # Generate a response
-    prompt = 'Using WebPilot, give me each of these items feature on the table at this url: ' + input
-    return llm(prompt)
+    prompt = 'Using WebPilot, give me each of these items feature on the table at this url: ' + input_url
+    response = llm(prompt)  # Assuming llm generates the response using the given prompt
+
+    # Save the response to a text file
+    output_file = "cash_flow_output.txt"
+    with open(output_file, 'w') as file:
+        file.write(response)
+
+    # Print the response to the console
+    print(response)
+
+    # Return the response (optional, you can remove this line if not needed)
+    return response
+
+
 
 # Page title
 st.title("WebPilot Revenue Viewer")
@@ -31,7 +46,6 @@ input_url = 'https://finance.yahoo.com/quote/LHV1T.TL/financials?p=LHV1T.TL'
 response_json = webpilot(input_url)
 response_json_balance = webpilot(input_url)
 
-text=Cash_flow_statement(input_url)
 
 df = pd.read_json(response_json)
 df_balance = pd.read_json(response_json_balance)
@@ -50,5 +64,7 @@ fig = px.bar(df_balance, x='Revenue in Euro', y='Year', title='Historical Revenu
 st.plotly_chart(fig)
 
 st.write("Cash flow statement")
-st.write(text)
+output_response = Cash_flow_statement(input_url)
+
+st.write(output_response)
 
