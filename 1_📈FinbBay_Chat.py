ad085fb5df247c0afe1196cc02848e2f5db2fe52
manelreghima <manelreghima@gmail.com>
2023-08-05 22:32:36 +0100

    update treemap

--- 1_📈FinbBay_Chat.py
+++ 1_📈FinbBay_Chat.py
@@ -376,9 +376,7 @@ if language_code=='en':
                  color_continuous_midpoint=np.average(market_data['stock_price'], weights=market_data['market_cap']))
 
     # Remove the 'id', 'parent', and 'label' from the hover tooltip for companies' symbols
-    fig.update_traces(hovertemplate='<b>%{customdata[0]}</b><br>%{label}<br>Stock Price: %{customdata[2]:.2f}(EUR)<br>Market Cap: %{customdata[3]:,.2f}(EUR) <br> Click to go to %{customdata[4]}<extra></extra>')
-
-    fig.update_layout(clickmode='event+select')
+    fig.update_traces(hovertemplate='<b>%{customdata[0]}</b><br>%{label}<br>Stock Price: %{customdata[2]:.2f}(EUR)<br>Market Cap: %{customdata[3]:,.2f}(EUR)')
 
     st.plotly_chart(fig)
 
